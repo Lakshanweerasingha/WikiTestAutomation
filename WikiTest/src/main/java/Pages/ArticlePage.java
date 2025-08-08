7214323b83d631b1e6ed4c155cf6ab7a8e60c456
LAKSHAN <LAKSHAN@DESKTOP-FGAR751>
2025-08-08 10:15:35 +0530

    fix

--- WikiTest/src/main/java/Pages/ArticlePage.java
+++ WikiTest/src/main/java/Pages/ArticlePage.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.pagefactory.AndroidFindBy;
 
 public class ArticlePage {
 	private AndroidDriver driver;
